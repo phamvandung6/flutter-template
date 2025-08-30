c2e208a73f440edee87eead8147fa33a5d1faeba
Dung Pham <van.phamdung2004@gmail.com>
2025-08-30 14:05:02 +0700

    refactor: delete typedef DataMap

--- lib/core/utils/typedef.dart
+++ lib/core/utils/typedef.dart
@@ -1,8 +1,6 @@
 import 'package:dartz/dartz.dart';
-
 import 'package:flutter_template/core/error/failures.dart';
 
 /// Type definitions for common return types
 typedef ResultFuture<T> = Future<Either<Failure, T>>;
 typedef ResultVoid = Future<Either<Failure, void>>;
-typedef DataMap = Map<String, dynamic>;
